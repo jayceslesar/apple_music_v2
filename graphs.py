d04f4497260ca413f798e2366805f50a6be7ea09
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-09-20 15:15:13 -0400

    cleanup

--- graphs.py
+++ graphs.py
@@ -31,7 +31,6 @@ num_artist_songs = 5
 
 def top_n_top_5(df, year, num_artists, num_artist_songs):
     artists = [pair[0] for pair in basic_music_stats.top_artists_year(df, year, num_artists)]
-    # del artists[artists.index('idk')]
     fig = make_subplots(rows=num_artists, cols=1, subplot_titles=(artists))
     for i, artist in enumerate(artists):
         xs, ys = [], []
@@ -71,52 +70,4 @@ def top_n_top_5(df, year, num_artists, num_artist_songs):
     fig.show()
 
 
-
 top_n_top_5(df, year, num_artists, num_artist_songs)
-
-
-
-
-
-
-
-
-
-
-def px_top_artist_year(df, year, n):
-    colors = []
-    x = []
-    y = []
-    for artist in basic_music_stats.top_artists_year(df, year, n)[::-1]:
-        try:
-            rgb = random.choice(color_stuff.compute_top_image_colors(artist[0]))
-        except:
-            rgb = [random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)]
-        colors.append('rgb(' + str(rgb[0]) + ', ' + str(rgb[1]) + ', ' + str(rgb[2]) + ')')
-        x.append(artist[0])
-        y.append(artist[1])
-    fig = go.Figure(data=[go.Bar(
-        x=y,
-        y=x,
-        orientation='h',
-        text=y,
-        marker=dict(color=colors)
-    )])
-
-    fig.update_layout({
-            'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-            'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-    })
-    fig.update_layout(
-    title="Play Counts for Top 10 Artists in 2020",
-    xaxis_title="Plays in 2020",
-    yaxis_title="Artist",
-    font=dict(
-        family="Courier New, monospace",
-        size=18
-    ))
-    fig.update_traces(textposition='outside')
-    fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
-    fig.show()
-
-
